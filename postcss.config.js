3a51927c44529f6dbce9f92ef614aea4ec11f673
knishina <knishina@yahoo-corp.jp>
2020-11-23 17:43:26 +0900

    Coded from initialization to Pre-rendering and Data Fetching

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    './pages/**/*.js',
+    './pages/**/*.tsx',
+    './components/**/*.js',
+    './components/**/*.tsx'
+  ],
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
+  ]
+}
